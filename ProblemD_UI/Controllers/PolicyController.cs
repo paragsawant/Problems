d1a2a744d1d857bb116102805241e880ca33722a
Parag Sawant <paragpps@gmail.com>
2017-12-08 22:10:33 -0800

    Checking IN New project for UI

---
+++ ProblemD_UI/Controllers/PolicyController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProblemD_UI.Controllers
+{
+    public class PolicyController : Controller
+    {
+        // GET: Policy
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Policy/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Policy/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Policy/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Policy/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Policy/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Policy/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Policy/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
